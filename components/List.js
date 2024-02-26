ca3464702190b51b99b221ad033799e290fcee4c
evan <malcomchege0582@gmail.com>
2024-02-26 12:25:30 +0300

    map out a list of data

--- components/List.js
+++ components/List.js
@@ -1,8 +1,21 @@
 import React from 'react'
 
-function List() {
+function List({people}) {
   return (
-    <div className='font-bold text-xl'>List Component</div>
+    <div>
+      {people.map((person)=>{
+        const {id,name,age,image}=person
+        return (
+          <article key={id} className='person flex items-center p-2 space-x-3'>
+            <img className='h-16 w-14 rounded-full' src={image} alt="" />
+            <div>
+              <h4 className='font-bold'>{name}</h4>
+              <p className='text-gray-400'>{age} years</p>
+            </div>
+          </article>
+        )
+      })}
+    </div>
   )
 }
 
--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,19 @@
 import Image from "next/image";
-import { Inter } from "next/font/google";
-import List from "@/components/List";
 
-const inter = Inter({ subsets: ["latin"] });
+import List from "@/components/List";
+import Data from "@/components/data";
+import { useState } from "react";
 
 export default function Home() {
+  const [people,setPeople] = useState(Data)
   return (
     <main>
 <div className="max-w-md mx-auto bg-white shadow-md rounded-md overflow-hidden mt-10">
   <div className="px-4 py-2 flex flex-col items-center">
-    <h3 className="font-semibold text-lg">0 birthdays today</h3>
-    <p className="text-gray-700">List component</p>
-    <button className="bg-pink-400 p-2 pl-7 pr-7 mt-4 text-white rounded-md"onClick={()=>{}}>ClearAll</button>
+    <h3 className="font-semibold text-lg">{people.length} birthdays today</h3>
+    <List people = {people}/>
+    <button className="bg-pink-400 p-2 pl-7 pr-7 mt-4 text-white rounded-md"onClick={()=>setPeople([])}>ClearAll</button>
+    
   </div>
 </div>
 
