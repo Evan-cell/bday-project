a89f6f03f184d8e1052cd6826f755753b2f8b5e1
evan <malcomchege0582@gmail.com>
2024-02-26 12:05:54 +0300

    data

--- pages/index.js
+++ pages/index.js
@@ -9,8 +9,9 @@ export default function Home() {
     <main>
 <div className="max-w-md mx-auto bg-white shadow-md rounded-md overflow-hidden mt-10">
   <div className="px-4 py-2 flex flex-col items-center">
-    <h3 className="font-semibold text-lg">Card Title</h3>
-    <p className="text-gray-700">Card content goes here.</p>
+    <h3 className="font-semibold text-lg">0 birthdays today</h3>
+    <p className="text-gray-700">List component</p>
+    <button className="bg-pink-400 p-2 pl-7 pr-7 mt-4 text-white rounded-md"onClick={()=>{}}>ClearAll</button>
   </div>
 </div>
 
